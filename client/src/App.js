869da3689bafcae4b7cde84bcf70f123e8d7a315
yakiv-k <yakiv.y.k@gmail.com>
2022-06-25 21:25:37 -0400

    Added base route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import TracksPage from "./pages/TracksPage/TracksPage";
 import "./styles/App.scss";
+import axios from "axios";
 
 function App() {
-  return <></>;
+  return <>
+  // <BrowserRouter>
+  // <Switch>
+  //   <Route path="/" exact component={TracksPage}></Route>
+  // </Switch>
+  // </BrowserRouter>
+  </>;
 }
 
 export default App;
