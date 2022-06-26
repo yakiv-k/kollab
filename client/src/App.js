8330dbf6e2881356856a2383b6b8943481e2c57d
yakiv-k <yakiv.y.k@gmail.com>
2022-06-25 21:36:38 -0400

    Create header component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import "./styles/App.scss";
 import axios from "axios";
 
 function App() {
-  return <>
-  // <BrowserRouter>
-  // <Switch>
-  //   <Route path="/" exact component={TracksPage}></Route>
-  // </Switch>
-  // </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={TracksPage}></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/TracksPage/TracksPage.js
@@ -0,0 +1,9 @@
+import "./TracksPage.scss";
+
+function TracksPage() {
+  return <>
+  
+  </>;
+};
+
+export default TracksPage;
