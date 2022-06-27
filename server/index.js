f57b66ea7acaf4da26c60dd54a6d623ae032b4f1
yakiv-k <yakiv.y.k@gmail.com>
2022-06-27 13:04:46 -0400

    Added routes to index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const producerRoutes = require("./routes/producerRoutes");
+const tracksRoutes = require("./routes/tracksRoutes");
+// const stemsRoutes = require("./routes/stemsRoutes");
 
+const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+app.use(cors());
+
+// ROUTES
+app.use("/producer", producerRoutes);
+app.use("/tracks", tracksRoutes);
+
+// LISTEN
 app.listen(PORT, () => {
   console.log("Server listening to http://localhost:" + PORT);
 });
