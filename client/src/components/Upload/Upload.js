05457c3bcd477f91781012963e0ce44b7dd2c72e
yakiv-k <yakiv.y.k@gmail.com>
2022-07-18 01:29:36 -0400

    Added component render animation to Upload

--- client/src/components/Upload/Upload.js
+++ client/src/components/Upload/Upload.js
@@ -1,154 +1,94 @@
-import { Component } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
 import "./Upload.scss";
-import { Link } from "react-router-dom";
 
-class Upload extends Component {
-  state = {
-    projectStemFiles: null,
-    projectImage: "",
-    track: null,
-    // isUploaded: false
-  };
+function Upload({ uploadFiles, trackHandler, ImageHandler, filesHandler }) {
+  // React spring animated
+  const [isVisible, setIsVisible] = useState(true);
+  const transition = useTransition(isVisible, {
+    from: { x: 0, y: 800, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: {},
+  });
 
-  // GRAB PROJECT FILE(S)
-  handleFileChange = (event) => {
-    event.preventDefault();
-    this.setState({
-      projectStemFiles: event.target.files,
-    });
-  };
+  return (
+    <>
+      <section onSubmit={uploadFiles} className="upload">
+        <h1 className="upload__title">Share</h1>
 
-  // GRAB IMAGE FILE
-  handleImageChange = (event) => {
-    event.preventDefault();
-    this.setState({
-      projectImage: event.target.files,
-    });
-  };
-
-  // GRAB TRACK
-  handleTrackChange = (event) => {
-    event.preventDefault();
-    this.setState({
-      track: event.target.files,
-    });
-  };
-  handleFilesUpload = (event) => {
-    event.preventDefault();
-
-    // NEW INSTANCE
-    const formData = new FormData();
-
-    // formData.append("name", event.target.name.value);
-    formData.append("title", event.target.title.value);
-    formData.append("bpm", event.target.bpm.value);
-    formData.append("caption", event.target.bpm.value);
-
-    for (const file of this.state.projectImage) {
-      formData.append("image", file);
-    }
-
-    Object.values(this.state.track).map((file) => {
-      formData.append("track", file);
-    });
-    Object.values(this.state.projectStemFiles).map((file) => {
-      formData.append("stems", file);
-    });
-
-    const token = sessionStorage.getItem("token");
-
-    axios
-      .post(
-        "http://localhost:8080/tracks",
-        formData,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        },
-        {
-          header: { "Content-Type": "multipart/form-data" },
-        }
-      )
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  render() {
-    return (
-      <>
-        <section onSubmit={this.handleFilesUpload} className="upload">
-          <h1 className="upload__title">Share</h1>
-          <div className="upload__content">
-            <form className="upload__form form" encType="multipart/form-data">
-              <label className="form__label" htmlFor="title">
-                Title
-              </label>
-              <input
-                className="form__input"
-                placeholder="Enter a title"
-                type="text"
-                name="title"
-                id="title"
-              ></input>
-              <label className="form__label" htmlFor="bpm">
-                BPM
-              </label>
-              <input
-                className="form__input"
-                placeholder="Enter track BPM"
-                type="text"
-                name="bpm"
-                id="bpm"
-              ></input>
-              <label className="form__label" htmlFor="caption">
-                Caption
-              </label>
-              <input
-                className="form__input"
-                placeholder="Enter a caption"
-                type="text"
-                name="caption"
-                id="caption"
-              ></input>
-              <label className="form__label">Choose track</label>
-              <input
-                className="form__input form__input--padding"
-                type="file"
-                name="track"
-                id="track"
-                accept=".wav, .mp3"
-                onChange={this.handleTrackChange}
-              ></input>
-              <label className="form__label">Add image</label>
-              <input
-                className="form__input form__input--padding"
-                type="file"
-                name="image"
-                id="image"
-                accept=".jpeg, .jpg, .png"
-                onChange={this.handleImageChange}
-              ></input>
-              <label className="form__label">Add project files</label>
-              <input
-                className="form__input form__input--padding"
-                type="file"
-                name="stems"
-                id="stems"
-                accept=".wav, .mp3"
-                multiple="multiple"
-                onChange={this.handleFileChange}
-              ></input>
-              {/* <Link to="/tracks"> */}
-              <button className="form__button">Upload</button>
-              {/* </Link> */}
-            </form>
-          </div>
-        </section>
-      </>
-    );
-  }
+        {transition((style, item) =>
+          item ? (
+            <animated.div style={style}className="upload__content">
+              <form className="upload__form form" encType="multipart/form-data">
+                <label className="form__label" htmlFor="title">
+                  Title
+                </label>
+                <input
+                  className="form__input"
+                  placeholder="Enter a title"
+                  type="text"
+                  name="title"
+                  id="title"
+                ></input>
+                <label className="form__label" htmlFor="bpm">
+                  BPM
+                </label>
+                <input
+                  className="form__input"
+                  placeholder="Enter track BPM"
+                  type="text"
+                  name="bpm"
+                  id="bpm"
+                ></input>
+                <label className="form__label" htmlFor="caption">
+                  Caption
+                </label>
+                <input
+                  className="form__input"
+                  placeholder="Enter a caption"
+                  type="text"
+                  name="caption"
+                  id="caption"
+                ></input>
+                <label className="form__label">Choose track</label>
+                <input
+                  className="form__input form__input--padding"
+                  type="file"
+                  name="track"
+                  id="track"
+                  accept=".wav, .mp3"
+                  onChange={trackHandler}
+                ></input>
+                <label className="form__label">Add image</label>
+                <input
+                  className="form__input form__input--padding"
+                  type="file"
+                  name="image"
+                  id="image"
+                  accept=".jpeg, .jpg, .png"
+                  onChange={ImageHandler}
+                ></input>
+                <label className="form__label">Add project files</label>
+                <input
+                  className="form__input form__input--padding"
+                  type="file"
+                  name="stems"
+                  id="stems"
+                  accept=".wav, .mp3"
+                  multiple="multiple"
+                  onChange={filesHandler}
+                ></input>
+                <button className="form__button">Upload</button>
+              </form>
+            </animated.div>
+          ) : (
+            ""
+          )
+        )}
+      </section>
+    </>
+  );
 }
 
 export default Upload;
--- client/src/pages/UploadPage/UploadPage.js
+++ client/src/pages/UploadPage/UploadPage.js
@@ -1,17 +1,92 @@
 import Upload from "../../components/Upload/Upload";
 import Header from "../../components/Header/Header";
+import { Component } from "react";
+import axios from "axios";
 
 import "./UploadPage.scss";
 
-function UploadPage () {
+class UploadPage extends Component {
+    state = {
+        projectStemFiles: null,
+        projectImage: "",
+        track: null,
+        // isUploaded: false
+      };
+    
+      // GRAB PROJECT FILE(S)
+      handleFileChange = (event) => {
+        event.preventDefault();
+        this.setState({
+          projectStemFiles: event.target.files,
+        });
+      };
+    
+      // GRAB IMAGE FILE
+      handleImageChange = (event) => {
+        event.preventDefault();
+        this.setState({
+          projectImage: event.target.files,
+        });
+      };
+    
+      // GRAB TRACK
+      handleTrackChange = (event) => {
+        event.preventDefault();
+        this.setState({
+          track: event.target.files,
+        });
+      };
+      handleFilesUpload = (event) => {
+        event.preventDefault();
+    
+        // NEW INSTANCE
+        const formData = new FormData();
+    
+        // formData.append("name", event.target.name.value);
+        formData.append("title", event.target.title.value);
+        formData.append("bpm", event.target.bpm.value);
+        formData.append("caption", event.target.bpm.value);
+    
+        for (const file of this.state.projectImage) {
+          formData.append("image", file);
+        }
+    
+        Object.values(this.state.track).map((file) => {
+          formData.append("track", file);
+        });
+        Object.values(this.state.projectStemFiles).map((file) => {
+          formData.append("stems", file);
+        });
+    
+        const token = sessionStorage.getItem("token");
+    
+        axios
+          .post(
+            "http://localhost:8080/tracks",
+            formData,
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            },
+            {
+              header: { "Content-Type": "multipart/form-data" },
+            }
+          )
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+    
+  render() {
     return (
-        <>
+      <>
         <Header />
         <section className="upload-page">
-        <Upload />
+          <Upload 
+          uploadFiles={this.handleFilesUpload} trackHandler={this.handleTrackChange} ImageHandler={this.handleImageChange} filesHandler={this.handleFileChange}/>
         </section>
-        </>
-    )
+      </>
+    );
+  }
 }
 
 export default UploadPage;
