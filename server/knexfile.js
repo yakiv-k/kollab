ff53ca0238e62d06ad1fc3f1775f26735de97f00
yakiv-k <yakiv.y.k@gmail.com>
2022-07-21 22:27:19 -0400

    Updated knexfile.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,15 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: 'kollab',
-      charset: 'utf8',
-    },
-  },
-  // PRODUCTION BELOW
-};
+process.env.NODE_ENV === "production"
+  ? (module.exports = {
+      production: {
+        client: "mysql",
+        connection: process.env.JAWSDB_URL,
+      },
+    })
+  : (module.exports = {
+      development: {
+        client: "mysql",
+        connection: {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: "kollab",
+          charset: "utf8",
+        },
+      },
+    });
+
+// const connections = {
+//   development: {
+//     client: 'mysql',
+//     connection: {
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: 'kollab',
+//       charset: 'utf8',
+//     },
+//   },
+//   production: {
+//     client: 'mysql',
+//     connection: process.env.JAWSDB_URL,
+//   },
+// };
+
+// console.log(connections.development);
+
+// module.exports =
+//   process.env.NODE_ENV === 'production'
+//     ? connections.production
+//     : connections;
