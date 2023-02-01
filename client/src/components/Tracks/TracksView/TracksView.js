d86aef52330d66a97426c5a2b1b177b1fbc81c1f
yakiv-k <yakiv.y.k@gmail.com>
2023-02-01 15:13:54 -0500

    TracksView endpoint fixed

--- client/src/components/Tracks/TracksView/TracksView.js
+++ client/src/components/Tracks/TracksView/TracksView.js
@@ -26,7 +26,7 @@ function TracksView(props) {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://kollab-production.up.railway.app/tracks/${track_id}`
+          `https://web-production-5250.up.railway.app/tracks/${track_id}`
         );
         setSelectedTrack(response.data.track);
         setSelectedTrackStems(response.data.stems);
